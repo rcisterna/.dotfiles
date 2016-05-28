2007fb4f976e8f0900153c0fa0a2a542edb5ea63
Ricardo Cisterna <r.cisternasantos@gmail.com>
2016-05-27 22:12:14 -0400

    brew_listdeps --include-build

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -6,7 +6,7 @@ function brew_listdeps()
     if brew list > /dev/null; then
         brew list | while read cask; do
             echo -ne "\x1B[1;34m$cask \x1B[0m"
-            brew deps $cask --include-optional --installed | awk '{printf(" %s", $0)}'
+            brew deps $cask --include-build --include-optional --installed | awk '{printf(" %s", $0)}'
             echo ""
         done
     fi
