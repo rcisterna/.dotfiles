294fe76611ac0f6d28f68b05e11f2978dabd03a2
Ricardo Cisterna <r.cisternasantos@gmail.com>
2016-05-27 21:13:31 -0400

    modificada funcion brew_orphans (ahora brew_listdeps)

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,9 +1,13 @@
 alias ls="ls -GFh"
 alias grep="grep --color=auto"
 
-function brew_orphans()
+function brew_listdeps()
 {
     if brew list > /dev/null; then
-        brew list -1 | while read cask; do echo -ne "\x1B[1;34m $cask \x1B[0m"; brew uses $cask --installed | awk '{printf(" %s ", $0)}'; echo ""; done
+        brew list | while read cask; do
+            echo -ne "\x1B[1;34m$cask \x1B[0m"
+            brew deps $cask --include-optional --installed | awk '{printf(" %s", $0)}'
+            echo ""
+        done
     fi
 }
