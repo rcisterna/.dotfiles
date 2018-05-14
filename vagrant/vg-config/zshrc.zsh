1abba331b5776b921ca433591b99343569c97ec0
Ricardo Cisterna <rcisterna@cadetech.cl>
2018-05-14 17:12:48 -0400

    vagrant config files udpated

--- vagrant/vg-config/zshrc.zsh
+++ vagrant/vg-config/zshrc.zsh
@@ -91,12 +91,12 @@ alias gt="g tag"
 # Prompt
 setopt prompt_subst
 
-local dot_start='━╾'
-local dot_ts='╼┥'
-local dot_end='┝╾'
-local sep='╌'
+local dot_start='——'
+local dot_ts='—|'
+local dot_end='|—'
+local sep='—'
 local current='%F{cyan}%n%f at %F{magenta}%m%f in %F{blue}%0~%f '
-local insert='%F{yellow}❯❯ %f'
+local insert='%F{yellow}» %f'
 local nl=$'\n'
 
 function __prompt()
@@ -111,7 +111,7 @@ function __prompt()
 
     datetime=" %D{%d-%m-%G} "
     let dots_count=${COLUMNS}-18 # dot_start(2) + dot_ts(2) + datetime(12) + dot_end(2)
-    while [[ $dots_count -gt 0 ]]; do
+    while [[ $dots_count -gt 1 ]]; do
         dynamic_dots="${dynamic_dots}$sep"
         let dots_count=${dots_count}-1
     done
@@ -123,24 +123,24 @@ function __prompt()
         git_rev="$(git rev-list --left-right @...@{u})"
 
         if git status -uno -s | grep -q . ; then
-            dirty=" %F{white}●%f"
+            dirty=" %F{white}×%f"
         fi
 
         behind="$(echo $git_rev | tr -d -c '>' | awk '{print length;}')"
         if [[ ! -z "${behind// }" ]]; then
-            behind=" %F{red}⇣$behind%f"
+            behind=" %F{red}-$behind%f"
         fi
 
         ahead="$(echo $git_rev | tr -d -c '<' | awk '{print length;}')"
         if [[ ! -z "${ahead// }" ]]; then
-            ahead=" %F{green}⇡$ahead%f"
+            ahead=" %F{green}+$ahead%f"
         fi
 
     fi
 
     PROMPT="$dot_start$dynamic_dots$dot_ts$datetime$dot_end$nl$current$branch$dirty$behind$ahead$nl$insert"
 }
-PROMPT2="%F{yellow}◀ %f"
-SPROMPT="%F{yellow}◀ %f Correct %F{red}%R%f to %F{green}%r%f [nyae]? "
+PROMPT2="%F{yellow}‹ %f"
+SPROMPT="%F{yellow}‹ %f Correct %F{red}%R%f to %F{green}%r%f [nyae]? "
 
 precmd () { __prompt }
