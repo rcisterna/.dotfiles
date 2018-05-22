a97b62246e1aaeab19b93d4b69664ffee0ef533d
Ricardo Cisterna <rcisterna@cadetech.cl>
2018-05-22 18:37:02 -0400

    git aliases updated

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -7,7 +7,9 @@ function touch {
     if (Test-Path -LiteralPath $Path) { (Get-Item -Path $Path).LastWriteTime = Get-Date }
     else { New-Item -Type File -Path $Path }
 }
+
 New-Alias -Name grep -Value Select-String
+New-ALias -Name open -Value start
 
 ## Agregar "activate" para conda envs
 if (Get-Command conda -errorAction SilentlyContinue)
@@ -22,68 +24,65 @@ $date_format="iso"
 
 New-Alias -Name g -Value git
 
-function git-add { git add $args }
+function git-add { g add $args }
 New-Alias -Name ga -Value git-add
 
-function git-branch { git branch $args }
+function git-branch { if (!$args) { g branch --all --verbose } else { g branch $args }  }
 New-Alias -Name gb -Value git-branch
 
-function git-blame { git blame --date=short $args }
+function git-blame { g blame --date=short $args }
 New-Alias -Name gbl -Value git-blame
 
 del alias:gc -Force
-function git-commit { git commit $args }
+function git-commit { g commit $args }
 New-Alias -Name gc -Value git-commit
 
-function git-checkout { git checkout $args }
+function git-checkout { g checkout $args }
 New-Alias -Name gch -Value git-checkout
 
-function git-cherry { git cherry-pick $args }
+function git-cherry { g cherry-pick $args }
 New-Alias -Name gchp -Value git-cherry
 
-function git-diff { git diff --patience $args }
+function git-diff { g diff --patience $args }
 New-Alias -Name gd -Value git-diff
 
-function git-fetch { git fetch $args }
+function git-fetch { g fetch $args }
 New-Alias -Name gf -Value git-fetch
 
 del alias:gl -Force
-function git-log { git log --graph --pretty=format:$commit_format --date=$date_format $args }
+function git-log { g log --graph --pretty=format:$commit_format --date=$date_format $args }
 New-Alias -Name gl -Value git-log
 
 del alias:gm -Force
-function git-merge { git merge $args }
+function git-merge { g merge $args }
 New-Alias -Name gm -Value git-merge
 
-function git-push { git push $args }
+function git-push { g push $args }
 New-Alias -Name gpush -Value git-push
 
-function git-pull { git pull $args }
+function git-pull { g pull $args }
 New-Alias -Name gpull -Value git-pull
 
-function git-reset { git reset $args }
+function git-reset { g reset $args }
 New-Alias -Name gr -Value git-reset
 
-function git-rm { git rm $args }
+function git-rm { g rm $args }
 New-Alias -Name grm -Value git-rm
 
-function git-status { git status --short $args }
+function git-status { g status --short $args }
 New-Alias -Name gs -Value git-status
 
-function git-status { git status --short --branch $args }
-New-Alias -Name gsb -Value git-status
-
-function git-show { git show --stat --pretty=format:$commit_format --date=$date_format $args }
+function git-show { g show --stat --pretty=format:$commit_format --date=$date_format $args }
 New-Alias -Name gsh -Value git-show
 
-function git-stash { git stash $args }
+function git-stash { g stash $args }
 New-Alias -Name gst -Value git-stash
 
-function git-tag { git tag $args }
+function git-tag { g tag $args }
 New-Alias -Name gt -Value git-tag
 
-function git-rebase { git rebase --interactive --autostash $args }
+function git-rebase { if (!$args) { g rebase --interactive --autostash } else { g rebase $args } }
 New-Alias -Name grb -Value git-rebase
 
-function git-remote { git remote --verbose $args }
+function git-remote { if (!$args) { g remote --verbose } else { g remote $args }  }
 New-Alias -Name grem -Value git-remote
