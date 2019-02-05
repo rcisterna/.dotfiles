0051ae72c65291c1fa17a1efdc09ca4bcb8a7bbe
Ricardo Cisterna <rcisterna@cadetech.cl>
2019-02-05 08:56:46 -0300

    minor alias changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,11 @@
 # Aliases generales
-alias ls="ls --group-directories-first --color=auto"
-alias cp="cp -i"
-alias df='df -h'
-alias free='free -m'
+alias ls="ls --human-readable --group-directories-first --color=auto"
+alias cp="cp --interactive"
+alias df='df --human-readable'
+alias free='free --mebi'
 alias tree="tree --dirsfirst -hpCL 2"
 alias grep="grep --color=auto"
 
+if hash xdg-open 2>/dev/null; then
+    alias open="xdg-open"
+fi
